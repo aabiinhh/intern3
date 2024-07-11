de18ab01d121488d3ef095f13d0a910b685a2553
abin y <abiny432@gmail.com>
2024-07-11 13:02:08 +0530

    mesaage to commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from bank_operation.bank import credit, debit,transfer
 
 
+class fruits :
+    def __init__(self, name, color):
+        
+
+class apple(fruits):
+    def my__type(self):
+        
